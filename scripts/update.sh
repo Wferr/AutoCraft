8421b23a90627bea69e339ca84218f58e7f928cb
Wferr <Wferr@users.noreply.github.com>
2015-06-19 11:54:41 -0700

    fix update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,36 +1,29 @@
 #!/bin/bash
 set -e
 
-#Used to call Update Loop
-
-function update-both {
-sh $HOME/AutoCraft/scripts/bungee-update.sh
-sh $HOME/AutoCraft/scripts/spigot-update.sh
-} >> $HOME/AutoCraft/tmp/update.log
-
-
 dialog --menu "Update Options" 10 60 20  1 Both 2 Spigot 3 Bungee 2>temp
  
- 
-# OK is pressed
 if [ "$?" = "0" ]
 then
         _return=$(cat temp)
  
         if [ "$_return" = "1" ]
         then
-        		update-both
-                dialog --title "Updating Both" --tailbox "$HOME/AutoCraft/tmp/update.log" 100 100
+        	echo "Updating Both"
+        	sh $HOME/AutoCraft/scripts/bungee-update.sh
+			sh $HOME/AutoCraft/scripts/spigot-update.sh
         fi
  
         if [ "$_return" = "2" ]
         then
-                dialog --title "Updating Spigot" --msgbox "$(sh $HOME/AutoCraft/scripts/spigot-update.sh)" 100 100
+        	echo "Updating Spigot"
+            sh $HOME/AutoCraft/scripts/spigot-update.sh
         fi
  
         if [ "$_return" = "3" ]
         then
-                dialog --title "Updating BungeeCord" --msgbox "$(sh $HOME/AutoCraft/scripts/bungee-update.sh)" 100 100
+        	echo "Updating BungeeCord"
+            sh $HOME/AutoCraft/scripts/bungee-update.sh
         fi
  
 else

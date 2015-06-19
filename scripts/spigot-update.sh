e1fcca771e195d7db0f92a9c9e3fa5eea38cad8b
Wferr <Wferr@users.noreply.github.com>
2015-06-19 13:14:32 -0700

    missing a space ,_,

--- scripts/spigot-update.sh
+++ scripts/spigot-update.sh
@@ -30,7 +30,7 @@ MD5_2=$(md5sum "$HOME/AutoCraft/resources/tmp/BuildTools.jar" | cut -d " " -f 1)
 echo "Old MD5 $MD5_1"
 echo "New MD5 $MD5_2"
 
-if [$MD5_1 != $MD5_2]
+if [$MD5_1 != $MD5_2 ]
 then
     echo "BuildTools is out of date! Copying over new BuildTools!"
 
